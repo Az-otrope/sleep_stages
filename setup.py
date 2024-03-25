a0d87539dfb6a6b48d52d2660889eb7bded9e7f6
Nguyen Pham <mia@Nguyens-MacBook-Pro.local>
2024-03-24 23:16:56 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.1.0",
+    description="A short description of the project.",
+    author="Mia",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    license="",
+)
